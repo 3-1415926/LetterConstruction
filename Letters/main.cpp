d40bddd9685f821136b8188a671e8fd5d6c6d273
None The Less <nonetheless@the.local>
2015-02-05 00:22:35 -0500

    Multi-page PDF support

--- Letters/main.cpp
+++ Letters/main.cpp
@@ -10,8 +10,9 @@
 #include "letters.h"
 
 int main() {
-  const double width = 612;
-  const double height = 792;
+  const double points_in_inch = 72;
+  const double width = 8.5 * points_in_inch;
+  const double height = 11 * points_in_inch;
   const double scale = 0.6;
   
   std::vector<Letter*> letters = {
@@ -24,22 +25,25 @@ int main() {
     new Э(), new Ю(), new Я(),
   };
   
+  std::string filename = "image.pdf";
+  Cairo::RefPtr<Cairo::PdfSurface> surface =
+    Cairo::PdfSurface::create(filename, width, height);
+
   auto letter = letters.begin();
   const int rows = 3;
   const int columns = 2;
-  for (int p = 1; letter != letters.end(); ++p) {
-    std::string filename = "image" + std::to_string(p) + ".pdf";
-    Cairo::RefPtr<Cairo::PdfSurface> surface =
-      Cairo::PdfSurface::create(filename, width, height);
+  while (letter != letters.end()) {
     for (int i = 0; i < rows && letter != letters.end(); ++i) {
       for (int j = 0; j < columns && letter != letters.end(); ++j, ++letter) {
         (*letter)->Draw(surface, scale,
           width * (j + 0.5) / columns, height * (i + 0.5) / rows);
       }
     }
-    surface.clear();
-    system(("open " + filename).c_str());
+    surface->show_page();
   }
+
+  surface.clear();
+  system(("open " + filename).c_str());
 
   return 0;
 }
